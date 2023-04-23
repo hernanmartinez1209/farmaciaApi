bdb699eff8221417e246b2d73a2ed54e908101e4
hernanmartinez1209 <108160168+hernanmartinez1209@users.noreply.github.com>
2023-04-23 15:46:03 -0600

    arreglos1.5

--- config.js
+++ config.js
@@ -1,29 +1,72 @@
-const config = {
-    port:process.env.PORT||5000,
-    nodeEnv: process.env.NODE_ENV || "prod",
-    db:{
-         prod: {
-            dialect: 'postgres',
-            host: process.env.DB_PROT_HOST,
-            port: process.envDB_PROT_PORT,
-            database: process.env.DB_PROT_NAME,
-            username: process.env.DN_PROT_USER,
-            password: process.env.DB_PROT_PASS,
-            //Extra configs
-            define: {
-              timestamps: true,
-              underscored: true,
-            },
-            //Esta configuracion es para produccion
-            dialectOptions: {
-              ssl: {
-                require: true,
-                rejectUnauthorized: false,
-              },
-            },
-          },
-    },
-    
-};
+const Sequelize = require('sequelize');
+const config = require('./config'); // importar la configuración del archivo config.js
 
-module.exports = config
+const environment = process.env.NODE_ENV || 'prod';
+const dbConfig = config.db[environment];
+
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  port: dbConfig.port,
+  dialect: dbConfig.dialect, // agregar el dialecto en la configuración
+  define: dbConfig.define,
+  dialectOptions: dbConfig.dialectOptions,
+});
+
+// exportar la instancia de Sequelize
+module.exports = sequelize;
+
+// const config = {
+//     port:process.env.PORT||5000,
+//     nodeEnv: process.env.NODE_ENV || "dev",
+//     db:{
+//         dev: {
+//             dialect: "postgres",
+//             host: "localhost",
+//             port: 5432,
+//             database: "farmaciaApi",
+//             username: "postgres",
+//             password: "node12",
+//             //Extra configs
+//             define: {
+//               timestamps: true,
+//               underscored: true,
+//             },
+//           },
+//          prod: {
+//             dialect: "postgres",
+//             host: process.env.DB_PROT_HOST,
+//             port: process.envDB_PROT_PORT,
+//             database: process.env.DB_PROT_NAME,
+//             username: process.env.DN_PROT_USER,
+//             password: process.env.DB_PROT_PASS,
+//             //Extra configs
+//             define: {
+//               timestamps: true,
+//               underscored: true,
+//             },
+//             //Esta configuracion es para produccion
+//             dialectOptions: {
+//               ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false,
+//               },
+//             },
+//           },
+//           test: {
+//             dialect: "postgres",
+//             host: "localhost",
+//             port: 5432,
+//             database: "farmaciaApi",
+//             username: "postgres",
+//             password: "node12",
+//             //Extra configs
+//             define: {
+//               timestamps: true,
+//               underscored: true,
+//             },
+//           },
+//     },
+    
+// };
+
+// module.exports = config
