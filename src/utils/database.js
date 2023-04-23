703c07377cba4eae6c3c797ccb7152edb66edc36
hernanmartinez1209 <108160168+hernanmartinez1209@users.noreply.github.com>
2023-04-23 15:50:00 -0600

    repon1

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,8 +2,8 @@ const {Sequelize} = require('sequelize');
 
 const config = require('../../config')
 
-//const enviorment = config.nodeEnv
+const enviorment = config.nodeEnv
 
-const db = new Sequelize(config.db[prod])
+const db = new Sequelize(config.db[enviorment])
 
  module.exports= db
