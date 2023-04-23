870746eb45d80bfa4b70a2888f3a4110035da402
hernanmartinez1209 <108160168+hernanmartinez1209@users.noreply.github.com>
2023-04-23 15:28:04 -0600

    areglos1.2

--- config.js
+++ config.js
@@ -2,19 +2,6 @@ const config = {
     port:process.env.PORT||5000,
     nodeEnv: process.env.NODE_ENV || "prod",
     db:{
-        dev: {
-            dialect: "postgres",
-            host: "localhost",
-            port: 5432,
-            database: "farmaciaApi",
-            username: "postgres",
-            password: "node12",
-            //Extra configs
-            define: {
-              timestamps: true,
-              underscored: true,
-            },
-          },
          prod: {
             dialect: "postgres",
             host: process.env.DB_PROT_HOST,
@@ -22,14 +9,6 @@ const config = {
             database: process.env.DB_PROT_NAME,
             username: process.env.DN_PROT_USER,
             password: process.env.DB_PROT_PASS,
-            dialectOptions:process.env.NODE_ENV === 'production'
-            ?{
-               ssl:{
-                require:true,
-                rejectUnauthorized:false
-               }
-            }:{}
-            ,
             //Extra configs
             define: {
               timestamps: true,
@@ -41,19 +20,6 @@ const config = {
                 require: true,
                 rejectUnauthorized: false,
               },
-            },
-          },
-          test: {
-            dialect: "postgres",
-            host: "localhost",
-            port: 5432,
-            database: "farmaciaApi",
-            username: "postgres",
-            password: "node12",
-            //Extra configs
-            define: {
-              timestamps: true,
-              underscored: true,
             },
           },
     },
